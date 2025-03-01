c8f800c17ceb85d1a43231ecda0c7e48c488f770
leezhenwang <1738789165@qq.com>
2025-03-01 11:09:09 +0800

    feat: 项目初始化

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>这里是首页</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+onMounted(() => {
+  console.log('mounted');
+})
+</script>
+<style>
+
+</style>
